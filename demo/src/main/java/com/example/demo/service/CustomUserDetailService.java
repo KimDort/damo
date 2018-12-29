0163ef4017dcde6d985884b5378150f962932cc8
youngjunkim <youngjunkim@localhost>
2018-12-30 01:03:54 +0900

    demo

---
+++ demo/src/main/java/com/example/demo/service/CustomUserDetailService.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+@Transactional
+public class CustomUserDetailService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username)
+				.orElseThrow(
+						() -> new UsernameNotFoundException("Email" + username + "not found")
+						);
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthorities(user));
+	}
+
+	private static Collection<? extends GrantedAuthority> getAuthorities(User user){
+		String[] userRoles = user.getRoles().stream().map((role) -> role.getName()).toArray(String[]::new);
+		Collection<GrantedAuthority> authority = AuthorityUtils.createAuthorityList(userRoles);
+		return authority;
+	}
+}
